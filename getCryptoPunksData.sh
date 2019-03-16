5f7ff35a34a7c2f09a86bf52d138a8c3e2d42749
BokkyPooBah <bokky.poobah@bokconsulting.com.au>
2019-03-16 11:07:33 +1100

    WIP

--- getCryptoPunksData.sh
+++ getCryptoPunksData.sh
@@ -8,8 +8,11 @@ echo "Starting" | tee $OUTPUTFILE
 
 geth attach << EOF | tee -a $OUTPUTFILE
 
+// 0
+var START = 2500;
 // cryptoPunks.totalSupply() = 10000
-var MAXINDEX=100;
+var END = 2600;
+console.log("RESULT: NOTE. Using only the indices between " + START + " and " + END + " for testing");
 
 loadScript("$ABI");
 
@@ -20,24 +23,19 @@ console.log("RESULT: name: " + cryptoPunks.name());
 console.log("RESULT: symbol: " + cryptoPunks.symbol());
 console.log("RESULT: decimals: " + cryptoPunks.decimals());
 console.log("RESULT: totalSupply: " + cryptoPunks.totalSupply());
-console.log("RESULT: NOTE. Using only the first " + MAXINDEX + " items for testing");
 console.log("RESULT: nextPunkIndexToAssign: " + cryptoPunks.nextPunkIndexToAssign());
 console.log("RESULT: allPunksAssigned: " + cryptoPunks.allPunksAssigned());
 console.log("RESULT: punksRemainingToAssign: " + cryptoPunks.punksRemainingToAssign());
 
-
-//mapping (address => uint) public addressToPunkIndex;
-// mapping (uint => address) public punkIndexToAddress;
-
-// mapping (address => uint256) public balanceOf;
-
 var accountsData = {};
 
 var i;
-for (i = 0; i < MAXINDEX; i++) {
+for (i = START; i < END; i++) {
   var address = cryptoPunks.punkIndexToAddress(i);
   accountsData[address] = 1;
   console.log("RESULT: punkIndexToAddress(" + i + "): " + cryptoPunks.punkIndexToAddress(i));
+  console.log("RESULT: punksOfferedForSale(" + i + "): " + cryptoPunks.punksOfferedForSale(i));
+  console.log("RESULT: punkBids(" + i + "): " + cryptoPunks.punkBids(i));
 }
 
 var accounts = Object.keys(accountsData).sort();
@@ -45,6 +43,7 @@ var accounts = Object.keys(accountsData).sort();
 accounts.forEach(function(e) {
   var balance = cryptoPunks.balanceOf(e);
   console.log("RESULT: balanceOf(" + e + "): " + balance);
+  console.log("RESULT: pendingWithdrawals(" + e + "): " + cryptoPunks.pendingWithdrawals(e));
 });
 
 
