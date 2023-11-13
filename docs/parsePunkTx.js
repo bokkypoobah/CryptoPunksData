29ec3298554ed43769075cd5d4f253e48e18f6fe
BokkyPooBah <bokky.poobah@bokconsulting.com.au>
2023-11-14 09:56:02 +1100

    categorising txs

--- docs/parsePunkTx.js
+++ docs/parsePunkTx.js
@@ -14,32 +14,61 @@ function parsePunkTx(txHash, events, addressToIndex) {
   }
 
   if (myCounter++ < 10) {
-    console.log("  " + txHash + ": " + JSON.stringify(events));    
+    console.log("  " + txHash + ": " + JSON.stringify(events));
   }
 
-  if (events.length > 0) {
-    // Assign
-    if (events[0][EVENTFIELD_TYPE] == 0) {
-      // console.log("Assign");
-      // V1
+  // indexToEventTypes: [
+  //   "Assign", // 0
+  //   "Transfer", // 1
+  //   "PunkTransfer", // 2
+  //   "PunkOffered", // 3
+  //   "PunkNoLongerForSale", // 4
+  //   "PunkBidEntered", // 5
+  //   "PunkBidWithdrawn", // 6
+  //   "PunkBought", // 7
+  //   "Approval", // 8
+  //   "ApprovalForAll", // 9
+  //   "OwnershipTransferred", // 10
+  //   "ProxyRegistered", // 11
+  // ],
+
+  // indexToTransactionTypes: [
+  //   "Reserve", // 0
+  //   "Claim", // 1
+  //   "Airdrop", // 2
+  //   "Transfer", // 3
+  //   "Offer", // 4
+  //   "RemoveOffer", // 5
+  //   "Purchase", // 6
+  //   "Bid", // 7
+  //   "RemoveBid", // 8
+  //   "AcceptBid", // 9
+  //   "Wrap", // 10
+  //   "Unwrap", // 11
+  //   "Approval", // 12
+  //   "ApprovalForAll", // 13
+  //   "OwnershipTransferred", // 14
+  //   "ProxyRegistered", // 15
+  // ],
+
+  const eventsLength = events.length;
+  if (eventsLength > 0) {
+    if (events[0][EVENTFIELD_TYPE] == 0) { // Assign
       if (events[0][EVENTFIELD_CONTRACT] == 1) {
-        // console.log("v1");
         if (events[0][3] == cryptoPunksDeployerIndex) {
-          console.log("v1 - LarvaLabs - first punkId: " + events[0][4] + ", count: " + events.length + " " + txHash);
-          return ['v1 LarvaLabs Reserve'];
+          return 0; // Reserve
         } else {
-          return ['v1 Claim'];
+          return 1; // Claim
         }
       } else if (events[0][EVENTFIELD_CONTRACT] == 2) {
-        // console.log("v2");
-        if (events[0][3] == cryptoPunksDeployerIndex) {
-          console.log("v2 - Airdrop v2 - first punkId: " + events[0][4] + ", count: " + events.length + " " + txHash);
-          return ['v2 Airdrop (LarvaLabs)'];
-        } else {
-          return ['v2 Airdrop'];
-          // console.log("--- UNEXPECTED ---")
-        }
+        return 2; // Airdrop
       }
+    }
+  }
+
+  if (eventsLength == 2) {
+    if (events[0][EVENTFIELD_TYPE] == 1 && events[1][EVENTFIELD_TYPE] == 2) { // Transfer & PunkTransfer
+      return 3; // Transfer
     }
   }
   return undefined;
