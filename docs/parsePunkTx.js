106793471ef759031fa119e82781f14f81cd69d4
BokkyPooBah <bokky.poobah@bokconsulting.com.au>
2023-11-14 13:38:06 +1100

    may be buggy

--- docs/parsePunkTx.js
+++ docs/parsePunkTx.js
@@ -69,7 +69,7 @@ function parsePunkTx(txHash, events, addressToIndex) {
     if (eventsLength == 2) { // V1 transferPunk and V2 without bids to cancel
       const secondEvent = events[1];
       if (firstEvent[EVENTFIELD_TYPE] == 1 && secondEvent[EVENTFIELD_TYPE] == 2) { // Transfer & PunkTransfer
-        return [3]; // Transfer
+        return [ 3, secondEvent[3], secondEvent[4], secondEvent[5] ]; // [ Transfer, from, to, punkId ]
       }
     }
 
@@ -77,17 +77,17 @@ function parsePunkTx(txHash, events, addressToIndex) {
       const secondEvent = events[1];
       const thirdEvent = events[2];
       if (firstEvent[EVENTFIELD_TYPE] == 4 && secondEvent[EVENTFIELD_TYPE] == 1 && thirdEvent[EVENTFIELD_TYPE] == 2) { // PunkNoLongerForSale & Transfer & PunkTransfer
-        return [3]; // Transfer
+        return [ 3, thirdEvent[3], thirdEvent[4], thirdEvent[5] ]; // [ Transfer, from, to, punkId ]
       }
     }
 
     if (eventsLength == 1) {
       if (firstEvent[EVENTFIELD_TYPE] == 1) { // Transfer (W1)
-        return [3]; // Transfer
+        return [ 3, firstEvent[3], firstEvent[4], firstEvent[5] ]; // [ Transfer, from, to, punkId ]
       } else if (firstEvent[EVENTFIELD_TYPE] == 3) { // PunkOffered
-        return [ 4, firstEvent[6], firstEvent[5], firstEvent[3], firstEvent[4] ]; // [Offer, from, to, punkId, amount]
+        return [ 4, firstEvent[6], firstEvent[5], firstEvent[3], firstEvent[4] ]; // [ Offer, from, to, punkId, amount ]
       } else if (firstEvent[EVENTFIELD_TYPE] == 4) { // PunkNoLongerForSale
-        return [5]; // Offer
+        return [5, firstEvent[4], firstEvent[3]]; // [ Offer, from, punkId ]
       } else if (firstEvent[EVENTFIELD_TYPE] == 5) { // PunkBidEntered
         return [7]; // Bid
       } else if (firstEvent[EVENTFIELD_TYPE] == 6) { // PunkBidWithdrawn
@@ -124,7 +124,7 @@ function parsePunkTx(txHash, events, addressToIndex) {
       const thirdEvent = events[2];
       const fourthEvent = events[3];
       if (firstEvent[EVENTFIELD_TYPE] == 1 && secondEvent[EVENTFIELD_TYPE] == 4 && thirdEvent[EVENTFIELD_TYPE] == 7 && fourthEvent[EVENTFIELD_TYPE] == 1) { // Transfer & PunkNoLongerForSale & PunkBought & Transfer
-        return [10]; // Wrap (V1)
+        return [ 10, firstEvent[3], fourthEvent[5] ]; // [ Wrap(V1), owner, punkId ]
       }
     }
 
