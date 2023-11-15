959a2d71b71ad1d86621b4f22b97538fb3141af2
BokkyPooBah <bokky.poobah@bokconsulting.com.au>
2023-11-15 14:28:43 +1100

    tidy

--- docs/parsePunkTx.js
+++ docs/parsePunkTx.js
@@ -57,22 +57,19 @@ function parsePunkTx(txHashIndex, events, addressToIndex) {
     if (firstEvent[EVENTFIELD_TYPE] == 0) { // Assign
       if (firstEvent[EVENTFIELD_CONTRACT] == 1) {
         if (firstEvent[3] == cryptoPunksDeployerIndex) {
-          // TODO: Fix
-          return [ 0, 0, firstEvent[3], events.map(e => e[4]) ]; // [ Reserve, from, to, [punkIds]]
+          return events.map(e => [ 0, 0, e[3], e[4] ]); // [[ Reserve, from, to, punkId]]
         } else {
-          // TODO: Fix
-          return [ 1, 0, firstEvent[3], firstEvent[4] ]; // [ Claim, from, to, punkId ]
+          return [[ 1, 0, firstEvent[3], firstEvent[4] ]]; // [[ Claim, from, to, punkId ]]
         }
       } else if (firstEvent[EVENTFIELD_CONTRACT] == 2) {
-        // TODO: Fix
-        return [ 2, 0, 0, events.map(e => [e[3], e[4]]) ]; // [ Airdrop, 0x0, 0x0, [to, punkId] ]
+        return events.map(e => [ 2, 0, e[3], e[4] ]); // [[ Airdrop, 0x0, to, punkId ]]
       }
     }
 
     if (eventsLength == 2) { // V1 transferPunk and V2 without bids to cancel
       const secondEvent = events[1];
       if (firstEvent[EVENTFIELD_TYPE] == 1 && secondEvent[EVENTFIELD_TYPE] == 2) { // Transfer & PunkTransfer
-        return [ 3, secondEvent[3], secondEvent[4], secondEvent[5] ]; // [ Transfer, from, to, punkId ]
+        return [[ 3, secondEvent[3], secondEvent[4], secondEvent[5] ]]; // [[ Transfer, from, to, punkId ]]
       }
     }
 
@@ -80,31 +77,31 @@ function parsePunkTx(txHashIndex, events, addressToIndex) {
       const secondEvent = events[1];
       const thirdEvent = events[2];
       if (firstEvent[EVENTFIELD_TYPE] == 4 && secondEvent[EVENTFIELD_TYPE] == 1 && thirdEvent[EVENTFIELD_TYPE] == 2) { // PunkNoLongerForSale & Transfer & PunkTransfer
-        return [ 3, thirdEvent[3], thirdEvent[4], thirdEvent[5] ]; // [ Transfer, from, to, punkId ]
+        return [[ 3, thirdEvent[3], thirdEvent[4], thirdEvent[5] ]]; // [[ Transfer, from, to, punkId ]]
       }
     }
 
     if (eventsLength == 1) {
       if (firstEvent[EVENTFIELD_TYPE] == 1) { // Transfer (W1)
-        return [ 3, firstEvent[3], firstEvent[4], firstEvent[5] ]; // [ Transfer, from, to, punkId ]
+        return [[ 3, firstEvent[3], firstEvent[4], firstEvent[5] ]]; // [[ Transfer, from, to, punkId ]]
       } else if (firstEvent[EVENTFIELD_TYPE] == 3) { // PunkOffered
-        return [ 4, firstEvent[6], firstEvent[5], firstEvent[3], firstEvent[4] ]; // [ Offer, from, to, punkId, amount ]
+        return [[ 4, firstEvent[6], firstEvent[5], firstEvent[3], firstEvent[4] ]]; // [[ Offer, from, to, punkId, amount ]]
       } else if (firstEvent[EVENTFIELD_TYPE] == 4) { // PunkNoLongerForSale
-        return [ 5, firstEvent[4], firstEvent[3] ]; // [ Offer, from, punkId ]
+        return [[ 5, firstEvent[4], , firstEvent[3] ]]; // [[ Offer, from, , punkId ]]
       } else if (firstEvent[EVENTFIELD_TYPE] == 5) { // PunkBidEntered
-        return [ 7, firstEvent[5], firstEvent[3], firstEvent[4] ]; // [ Bid, from, punkId, amount ]
+        return [[ 7, firstEvent[5], , firstEvent[3], firstEvent[4] ]]; // [[ Bid, from, , punkId, amount ]]
       } else if (firstEvent[EVENTFIELD_TYPE] == 6) { // PunkBidWithdrawn
-        return [ 8, firstEvent[5], firstEvent[3], firstEvent[4] ]; // [ RemoveBid, from, punkId, amount ]
+        return [[ 8, firstEvent[5], , firstEvent[3], firstEvent[4] ]]; // [[ RemoveBid, from, , punkId, amount ]]
       } else if (firstEvent[EVENTFIELD_TYPE] == 8) { // Approval
-        return [ 12, firstEvent[3], firstEvent[4], firstEvent[5] ]; // [ Approval, owner, approved, tokenId ]
+        return [[ 12, firstEvent[3], firstEvent[4], firstEvent[5] ]]; // [[ Approval, owner, approved, tokenId ]]
         // w1 w2 event Approval(address indexed owner, address indexed approved, uint256 indexed tokenId);
       } else if (firstEvent[EVENTFIELD_TYPE] == 9) { // ApprovalForAll
-        return [ 13, firstEvent[3], firstEvent[4], firstEvent[5] ]; // [ ApprovalForAll, owner, operator, approved ]
+        return [[ 13, firstEvent[3], firstEvent[4], firstEvent[5] ]]; // [[ ApprovalForAll, owner, operator, approved ]]
         // w1 w2 event ApprovalForAll(address indexed owner, address indexed operator, bool approved);
       } else if (firstEvent[EVENTFIELD_TYPE] == 10) { // OwnershipTransferred
-        return [ 14, firstEvent[3], firstEvent[4] ]; // [ OwnershipTransferred, from, to ]
+        return [[ 14, firstEvent[3], firstEvent[4] ]]; // [ OwnershipTransferred, from, to ]
       } else if (firstEvent[EVENTFIELD_TYPE] == 11) { // ProxyRegistered
-        return [ 15, firstEvent[3], firstEvent[4] ]; // [ ProxyRegistered, user, proxy ]
+        return [[ 15, firstEvent[3], firstEvent[4] ]]; // [ ProxyRegistered, user, proxy ]
       }
     }
 
@@ -112,14 +109,14 @@ function parsePunkTx(txHashIndex, events, addressToIndex) {
       const secondEvent = events[1];
       const thirdEvent = events[2];
       if (firstEvent[EVENTFIELD_TYPE] == 1 && secondEvent[EVENTFIELD_TYPE] == 4 && thirdEvent[EVENTFIELD_TYPE] == 7) { // Transfer & PunkNoLongerForSale & PunkBought
-        return [ 6, thirdEvent[6], thirdEvent[5], thirdEvent[3], thirdEvent[4] ]; // [ Purchase, from, to, punkId, amount ]
+        return [[ 6, thirdEvent[5], thirdEvent[6], thirdEvent[3], thirdEvent[4] ]]; // [[ Purchase, from, to, punkId, amount ]]
       }
     }
 
     if (eventsLength == 2) {
       const secondEvent = events[1];
       if (firstEvent[EVENTFIELD_TYPE] == 1 && secondEvent[EVENTFIELD_TYPE] == 7) { // Transfer & PunkBought
-        return [ 9, secondEvent[5], secondEvent[6], secondEvent[3], secondEvent[4]]; // [ AcceptBid, from, to, punkId, amount ]
+        return [[ 9, secondEvent[5], secondEvent[6], secondEvent[3], secondEvent[4]]]; // [[ AcceptBid, from, to, punkId, amount ]]
       }
     }
 
@@ -129,7 +126,7 @@ function parsePunkTx(txHashIndex, events, addressToIndex) {
       const thirdEvent = events[2];
       const fourthEvent = events[3];
       if (firstEvent[EVENTFIELD_TYPE] == 1 && secondEvent[EVENTFIELD_TYPE] == 4 && thirdEvent[EVENTFIELD_TYPE] == 7 && fourthEvent[EVENTFIELD_TYPE] == 1) { // Transfer & PunkNoLongerForSale & PunkBought & Transfer
-        return [ 10, firstEvent[3], fourthEvent[5], , 1 ]; // [ Wrap(V1), owner, punkId ]
+        return [[ 10, firstEvent[3], firstEvent[3], fourthEvent[5] ]]; // [[ Wrap(V1), owner, owner, punkId ]]
       }
     }
 
@@ -139,7 +136,7 @@ function parsePunkTx(txHashIndex, events, addressToIndex) {
       const thirdEvent = events[2];
       const fourthEvent = events[3];
       if (firstEvent[EVENTFIELD_TYPE] == 8 && secondEvent[EVENTFIELD_TYPE] == 1 && thirdEvent[EVENTFIELD_TYPE] == 1 && fourthEvent[EVENTFIELD_TYPE] == 2) { // Approval & Transfer & Transfer & PunkTransfer
-        return [11, fourthEvent[4], fourthEvent[5] ]; // [ Unwrap(V1), owner, punkId ]
+        return [[11, fourthEvent[4], fourthEvent[4], fourthEvent[5] ]]; // [[ Unwrap(V1), owner, owner, punkId ]]
       }
     }
 
@@ -148,7 +145,7 @@ function parsePunkTx(txHashIndex, events, addressToIndex) {
       const secondEvent = events[1];
       const thirdEvent = events[2];
       if (firstEvent[EVENTFIELD_TYPE] == 1 && secondEvent[EVENTFIELD_TYPE] == 2 && thirdEvent[EVENTFIELD_TYPE] == 1) { // Transfer & PunkTransfer & Transfer
-        return [ 10, thirdEvent[4], thirdEvent[5] ]; // [ Wrap(V2), owner, punkId ]
+        return [[ 10, thirdEvent[4], thirdEvent[4], thirdEvent[5] ]]; // [[ Wrap(V2), owner, owner, punkId ]]
       }
     }
 
@@ -157,10 +154,10 @@ function parsePunkTx(txHashIndex, events, addressToIndex) {
       const secondEvent = events[1];
       const thirdEvent = events[2];
       if (firstEvent[EVENTFIELD_TYPE] == 1 && secondEvent[EVENTFIELD_TYPE] == 1 && thirdEvent[EVENTFIELD_TYPE] == 2) { // Transfer & Transfer & PunkTransfer
-        return [ 11, thirdEvent[4], thirdEvent[5] ]; // [ Unwrap(V2), owner, punkId ]
+        return [[ 11, thirdEvent[4], thirdEvent[4], thirdEvent[5] ]]; // [[ Unwrap(V2), owner, owner, punkId ]]
       }
     }
 
   }
-  return [undefined];
+  return undefined;
 }
